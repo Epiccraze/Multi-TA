bab35f6c60aa9fcf06fe76873d1dd7ce0b8438a9
Epiccraze <Pal_rao@yahoo.com>
2026-06-08 20:29:35 -0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,5 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+HF_API_KEY = os.getenv("Hhf_oebntGbchrPFnxrEmUFElcKdvoFWNAYSRE", "")
+GROQ_API_KEY = os.getenv("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", "")
